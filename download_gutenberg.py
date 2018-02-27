58dd9200f54b94d4a113075e01fbb8c3208b50cd
eardil <irving@datank.ai>
2018-02-27 16:21:55 -0600

    added script for downloading books

---
+++ download_gutenberg.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from gutenberg.acquire import load_etext
+from gutenberg.cleanup import strip_headers
+
+
+def download_gutenberg(gutenberg_id):
+    return strip_headers(load_etext(gutenberg_id)).strip()
+
+
+gtn = pd.read_csv('df_libros.csv')
+
+dwnld = os.listdir('books')
+dwnld = [int(s.strip('.txt')) for s in dwnld]
+
+gtn_id = gtn.gutenberg_id.unique().tolist()
+gtn_id = list(set(gtn_id) - set(dwnld))
+for i in tqdm(range(len(gtn_id))):
+    try:
+        with open('books/' + str(gtn_id[i]) + '.txt', 'w') as f:
+            f.write(download_gutenberg(gtn_id[i]))
+    except:
+        print("No se bajó:", gtn_id[i])
