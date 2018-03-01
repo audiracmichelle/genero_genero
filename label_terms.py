63e2c5cb1e389292a06f777949ab2b80d6475cd3
eardil <irving@datank.ai>
2018-03-01 17:59:08 -0600

    added labeling script

---
+++ label_terms.py
@@ -0,0 +1,19 @@
+from tqdm import tqdm
+import pandas as pd
+import os
+
+df_books = pd.read_csv('params/df_libros.csv')
+with open('params/terms.txt') as f:
+    terms = f.read().splitlines()
+
+list_books = os.listdir('books')
+
+for b in tqdm(range(len(list_books))):
+    name = list_books[b]
+    ctg = df_books[df_books.gutenberg_id == int(name.strip('.txt'))].clave
+    with open('books/' + name) as f:
+        book_aux = f.read()
+    for t in terms:
+        book_aux = book_aux.replace(t, t + '_' + ctg.iloc[0])
+    with open('books_labeled/' + name, 'w') as f:
+        f.write(book_aux)
