1d05f8a485e647a9e46f627eac227ff93d54e319
eardil <irving@datank.ai>
2018-03-06 11:45:16 -0600

    modularized label_terms

--- label_terms.py
+++ label_terms.py
@@ -3,11 +3,11 @@ import pandas as pd
 import os
 import re
 
-df_books = pd.read_csv('params/df_libros.csv')
-with open('params/terms.txt') as f:
-    terms = f.read().splitlines()
 
-list_books = os.listdir('books')
+def get_data(terms_file):
+    with open(terms_file) as f:
+        terms = f.read().splitlines()
+    return os.listdir('books'), terms
 
 
 def replace_term(term, cat, text):
@@ -17,12 +17,19 @@ def replace_term(term, cat, text):
         ' ' + term + '_' + cat + ' ')
 
 
-for b in tqdm(range(len(list_books))):
-    name = list_books[b]
-    ctg = df_books[df_books.gutenberg_id == int(name.strip('.txt'))].clave
-    with open('books/' + name) as f:
-        book_aux = f.read()
-    for t in terms:
-        book_aux = replace_term(t, ctg.iloc[0], book_aux)
-    with open('books_labeled/' + name, 'w') as f:
-        f.write(book_aux)
+def main_replace(list_books, terms):
+    for b in tqdm(range(len(list_books))):
+        name = list_books[b]
+        ctg = df_books[df_books.gutenberg_id == int(name.strip('.txt'))].clave
+        with open('books/' + name) as f:
+            book_aux = f.read()
+        for t in terms:
+            book_aux = replace_term(t, ctg.iloc[0], book_aux)
+        with open('books_labeled/' + name, 'w') as f:
+            f.write(book_aux)
+
+
+if __name__ == '__main__':
+    df_books = pd.read_csv('params/df_libros.csv')
+    list_books, terms = get_data('params/terms.txt')
+    main_replace(list_books, terms)
