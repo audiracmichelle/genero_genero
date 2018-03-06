a6422c452d55a0a8f9ebae3db6af6c8697011d73
eardil <irving@datank.ai>
2018-03-06 12:34:34 -0600

    modularize download

--- download_gutenberg.py
+++ download_gutenberg.py
@@ -9,16 +9,19 @@ def download_gutenberg(gutenberg_id):
     return strip_headers(load_etext(gutenberg_id)).strip()
 
 
-gtn = pd.read_csv('df_libros.csv')
+def download_books(gtn_id):
+    gtn_id = list(set(gtn_id) - set(dwnld))
+    for i in tqdm(range(len(gtn_id))):
+        try:
+            with open('books/' + str(gtn_id[i]) + '.txt', 'w') as f:
+                f.write(download_gutenberg(gtn_id[i]))
+        except:
+            print("No se bajó:", gtn_id[i])
 
-dwnld = os.listdir('books')
-dwnld = [int(s.strip('.txt')) for s in dwnld]
 
-gtn_id = gtn.gutenberg_id.unique().tolist()
-gtn_id = list(set(gtn_id) - set(dwnld))
-for i in tqdm(range(len(gtn_id))):
-    try:
-        with open('books/' + str(gtn_id[i]) + '.txt', 'w') as f:
-            f.write(download_gutenberg(gtn_id[i]))
-    except:
-        print("No se bajó:", gtn_id[i])
+if __name__ == '__main__':
+    gtn = pd.read_csv('df_libros.csv')
+    dwnld = os.listdir('books')
+    dwnld = [int(s.strip('.txt')) for s in dwnld]
+    gtn_id = gtn.gutenberg_id.unique().tolist()
+    download_books(gtn_id)
